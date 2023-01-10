fdfec5d7bbf07cb443095fd55cbab8ac91710f87
Juan5987 <juan.machado.5987@gmail.com>
2023-01-10 18:23:34 +0100

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuopen] = React.useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <span className={styles.title}>monpoker</span>
+      <ul className={styles.list}>
+        <li>
+          <Link href='/' className={styles.element}>
+            accueil
+          </Link>
+        </li>
+
+        <li>
+          <Link href='/tournaments' className={styles.element}>
+            tournois
+          </Link>
+        </li>
+        <li>
+          <Link href='/chips' className={styles.element}>
+            jetons
+          </Link>
+        </li>
+
+        <li>
+          <Link href='/account' className={styles.element}>
+            compte
+          </Link>
+        </li>
+
+        <li>
+          <Link href='/login' className={styles.element}>
+            connexion
+          </Link>
+        </li>
+      </ul>
+      <div
+        onClick={() => setIsMenuopen(!isMenuOpen)}
+        className={isMenuOpen ? styles.burgerOpen : styles.burger}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={isMenuOpen ? styles.listMobile : styles.listMobileClose}>
+        <li className={styles.li}>
+          <Link href='/' className={styles.elementMobile}>
+            accueil
+          </Link>
+        </li>
+
+        <li className={styles.li}>
+          <Link href='/tournaments' className={styles.elementMobile}>
+            tournois
+          </Link>
+        </li>
+        <li className={styles.li}>
+          <Link href='/chips' className={styles.elementMobile}>
+            jetons
+          </Link>
+        </li>
+
+        <li className={styles.li}>
+          <Link href='/account' className={styles.elementMobile}>
+            compte
+          </Link>
+        </li>
+
+        <li className={styles.li}>
+          <Link href='/login' className={styles.elementMobile}>
+            connexion
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
